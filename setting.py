2344dab7e44cacf3a22f99f0282a14469a19c2fa
unknown <vandrager21@gmail.com>
2021-11-06 20:28:17 +0900

    game test

---
+++ setting.py
@@ -0,0 +1,17 @@
+# name = str(input("이름을 입력하시오: "))
+# age = int(input("나이를 입력하시오: "))
+# sex = str(input("성별을 입력하시오(남성, 여성): "))
+# if sex not in ["남성", "여성"]:
+#     sex = "미분류"
+# print(f"\"입력완료: {name}({age}, {sex})\"")
+
+# stat_str = 1
+# stat_int = 1
+# stat_hp = 50
+# stat_mp = 30
+
+# a = int(input()); b = int(input()); print(a+b)
+
+a, b = map(int, input().split())
+# hello world
+print(a+b) 
